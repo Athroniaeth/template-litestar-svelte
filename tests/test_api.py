4e3be829ae5ca8c654c0715c08ba462feadd4b53
Athroniaeth <pierre.chaumont@hotmail.fr>
2026-09-01 11:29:10 +0200

    style: add ruff & pyrefly and lint/format code

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,8 +3,6 @@ from litestar.status_codes import HTTP_200_OK
 from litestar.testing import AsyncTestClient
 
 
-
-
 async def test_health_check(client: AsyncTestClient):
     response = await client.get("/api/health")
     assert response.status_code == HTTP_200_OK
